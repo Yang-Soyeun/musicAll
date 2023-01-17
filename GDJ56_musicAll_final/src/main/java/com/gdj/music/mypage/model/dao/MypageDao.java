363ae4a1d17566ed7ef1c89a4d8cf627ffa953ef
Nari <Nari@DESKTOP-VHJ0A3C>
2023-01-18 00:05:52 +0900

    포인트출력중

--- GDJ56_musicAll_final/src/main/java/com/gdj/music/mypage/model/dao/MypageDao.java
+++ GDJ56_musicAll_final/src/main/java/com/gdj/music/mypage/model/dao/MypageDao.java
@@ -1,5 +1,11 @@
 package com.gdj.music.mypage.model.dao;
 
-public interface MypageDao {
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.gdj.music.reservation.model.vo.Point;
+
+public interface MypageDao {
+	List<Point> selectPointList(SqlSessionTemplate session);//포인트리스트 출력
 }
