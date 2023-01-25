ae9ea8dee11b58d1c17b217bf0f03e5e652ab4be
Nari <Nari@DESKTOP-VHJ0A3C>
2023-01-25 14:16:29 +0900

    1대1문의 검색 ajax처리

--- GDJ56_musicAll_final/src/main/java/com/gdj/music/question/model/vo/Question.java
+++ GDJ56_musicAll_final/src/main/java/com/gdj/music/question/model/vo/Question.java
@@ -2,7 +2,7 @@ package com.gdj.music.question.model.vo;
 
 import java.sql.Date;
 
-import com.gdj.music.pay.model.vo.Pay;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +17,7 @@ public class Question {
 	private int qsNo;
 	private String qsTitle;
 	private String qsContent;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
 	private Date qsDate;
 	private String qsHeadTitle;
 	private String qsResult;
