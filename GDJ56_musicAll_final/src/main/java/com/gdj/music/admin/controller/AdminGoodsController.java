69c22b4b519bd07583dfa17da512f8284c009282
USER <USER@222.234.129.214>
2023-02-08 01:08:35 +0900

    [sb] 굿즈 수정 삭제 구현 중

--- GDJ56_musicAll_final/src/main/java/com/gdj/music/admin/controller/AdminGoodsController.java
+++ GDJ56_musicAll_final/src/main/java/com/gdj/music/admin/controller/AdminGoodsController.java
@@ -157,5 +157,81 @@ public class AdminGoodsController {
 		
 	}
 	
+	//굿즈 수정
+	@RequestMapping("/updateGoods.do")
+	@ResponseBody
+	public int updateGoods(HttpSession session, MultipartFile upFile, 
+			MultipartFile upFile2,
+			Goods goods, HttpServletResponse response) throws Exception {
+		
+		//파일 업로드처리하기
+		String path=session.getServletContext().getRealPath("/resources/upload/goods/");
+		
+		File dir=new File(path);
+		if(!dir.exists()) dir.mkdirs();
+		List<GoodsImg> files=new ArrayList();
+		
+		System.out.println(upFile);
+		System.out.println(upFile2);
+		System.out.println(goods);
+		
+		//썸네일 
+		//리네임드규칙을 생성하기
+		if(upFile!=null&&!upFile.isEmpty()) {
+			String originalFileName=upFile.getOriginalFilename();
+			String ext=originalFileName.substring(originalFileName.lastIndexOf("."));
+			//중복되지않는 이름 설정하는 값지정하기
+			SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
+			int rnd=(int)(Math.random()*10000)+1;
+			String renameFile=sdf.format(System.currentTimeMillis())+"_"+rnd+ext;
+			//파일 업로드하기
+			try {
+				upFile.transferTo(new File(path+renameFile));
+				files.add(
+					GoodsImg.builder()
+					.sumImage("ok")
+					.imName(renameFile)
+					.build());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}			
+		//일반사진
+		//리네임드규칙을 생성하기
+		if(upFile2!=null&&!upFile2.isEmpty()) {
+			String originalFileName2=upFile2.getOriginalFilename();
+			String ext2=originalFileName2.substring(originalFileName2.lastIndexOf("."));
+			//중복되지않는 이름 설정하는 값지정하기
+			SimpleDateFormat sdf2=new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
+			int rnd2=(int)(Math.random()*10000)+1;
+			String renameFile2=sdf2.format(System.currentTimeMillis())+"_"+rnd2+ext2;
+			//파일 업로드하기
+			try {
+				upFile2.transferTo(new File(path+renameFile2));
+				files.add(
+						GoodsImg.builder()
+						.imName(renameFile2)
+						.build());
+				}catch(IOException e) {
+						e.printStackTrace();
+				}					
+		}
+		
+		Goods g = Goods.builder()
+				.gdName(goods.getGdName())
+				.gdPrice(goods.getGdPrice())
+				.gdContent(goods.getGdContent())
+				.gdCom(goods.getGdCom())
+				.gdCount(goods.getGdCount())
+				.gdTag(goods.getGdTag())
+				.mCode(goods.getMCode())
+				.build();
+
+
+		int result = service.updateGoods(g, files);
+		
+		return result;
+	}
+	
 
 }
